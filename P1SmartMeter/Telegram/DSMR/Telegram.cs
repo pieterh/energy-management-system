a9756fd4ce754bbb6b5cf7ddfeedb492075b5d88
pieterh <5267445+pieterh@users.noreply.github.com>
2021-03-17 18:56:49 +0100

    Start reading P1 smartmeter
    Signed-off-by: pieterh <5267445+pieterh@users.noreply.github.com>

---
+++ P1SmartMeter/Telegram/DSMR/Telegram.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace P1SmartMeter.Telegram.DSMR
+{
+    public class DSMRTelegram : TelegramBase
+    {
+        public DSMRTelegram(string raw) : base(TelegramDefinition.Instance, raw)
+        {
+            Timestamp = GetValue<DateTime?>(TelegramDefinition.Timestamp);
+            Electricity1FromGrid = GetValue<(double, string)?>(TelegramDefinition.Electricity1ToClient)?.Item1;
+            Electricity2FromGrid = GetValue<(double, string)?>(TelegramDefinition.Electricity2ToClient)?.Item1;
+            Electricity1ToGrid = GetValue<(double, string)?>(TelegramDefinition.Electricity1FromClient)?.Item1;
+            Electricity2ToGrid = GetValue<(double, string)?>(TelegramDefinition.Electricity2FromClient)?.Item1;
+
+            PowerUsedL1 = GetValue<(double, string)?>(TelegramDefinition.PowerUsedL1)?.Item1;
+            PowerUsedL2 = GetValue<(double, string)?>(TelegramDefinition.PowerUsedL2)?.Item1;
+            PowerUsedL3 = GetValue<(double, string)?>(TelegramDefinition.PowerUsedL3)?.Item1;
+
+            PowerReturnedL1 = GetValue<(double, string)?>(TelegramDefinition.PowerReturnedL1)?.Item1;
+            PowerReturnedL2 = GetValue<(double, string)?>(TelegramDefinition.PowerReturnedL2)?.Item1;
+            PowerReturnedL3 = GetValue<(double, string)?>(TelegramDefinition.PowerReturnedL3)?.Item1;
+
+            TariffIndicator = GetValue<int?>(TelegramDefinition.TariffIndicator);
+            ActualPower = GetValue<(double, string)?>(TelegramDefinition.ActualPower)?.Item1;
+            TextMessage = GetValue<string>(TelegramDefinition.TextMessage);
+        }
+
+        public DateTime? Timestamp { get; }
+
+        public string TextMessage { get; }
+
+        public double? ActualPower { get; }
+
+        public int? TariffIndicator { get; }
+
+        public double? Electricity1FromGrid { get; }
+
+        public double? Electricity2FromGrid { get; }
+
+        public double? Electricity1ToGrid { get; }
+
+        public double? Electricity2ToGrid { get; }
+
+        public double? PowerUsedL1 { get; }
+        public double? PowerUsedL2 { get; }
+        public double? PowerUsedL3 { get; }
+
+        public double? PowerReturnedL1 { get; }
+        public double? PowerReturnedL2 { get; }
+        public double? PowerReturnedL3 { get; }
+
+        public override string ToString()
+        {
+            return $"Timestamp: {Timestamp}\nE1: {Electricity1FromGrid}\nE2: {Electricity1FromGrid}\nTariff: {TariffIndicator}\nActualPower: {ActualPower}\nTextMessage: {TextMessage}";
+        }
+    }
+}
---
+++ P1SmartMeter/Telegram/TelegramField.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace P1SmartMeter.Telegram
+{
+    public class TelegramField
+    {
+        private readonly TelegramFieldDefinition _definition;
+
+        public TelegramField(TelegramFieldDefinition definition)
+        {
+            _definition = definition;
+        }
+
+        public IList<object> Values { get; } = new List<object>();
+        public string SingleValue => string.Join("; ", Values);
+
+        public void AddValue(string value)
+        {
+            var parsedValue = _definition.ParseValue(Values.Count, value);
+            Values.Add(parsedValue);
+        }
+
+        public override string ToString()
+        {
+            return $"{_definition.GetLabel()}: {SingleValue}";
+        }
+
+        public bool IsField(string fieldCode)
+        {
+            return _definition.Code == fieldCode;
+        }
+    }
+}
