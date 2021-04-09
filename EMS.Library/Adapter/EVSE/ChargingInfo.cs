fe50f39596a83a428da50f4149fc93e3286c27b0
pieterh <5267445+pieterh@users.noreply.github.com>
2021-04-09 21:53:04 +0200

    implement maxsolar and maxcharge calculations

---
+++ EMS.Library/Adapter/EVSE/ChargingInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace EMS.Library.Adapter.EVSE
+{
+    public class ChargingInfo
+    {
+        public float CurrentL1 { get; set; }
+        public float CurrentL2 { get; set; }
+        public float CurrentL3 { get; set; }
+
+        public float VoltageL1 { get; set; }
+        public float VoltageL2 { get; set; }
+        public float VoltageL3 { get; set; }
+
+        public ChargingInfo(float c1, float c2, float c3, float v1, float v2, float v3)
+        {
+            CurrentL1 = c1;
+            CurrentL2 = c2;
+            CurrentL3 = c3;
+
+            VoltageL1 = v1;
+            VoltageL2 = v2;
+            VoltageL3 = v3;
+        }
+    }
+}
