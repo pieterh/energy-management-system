ecb5952e656f6b464641841bc577e02183a4b76d
pieterh <5267445+pieterh@users.noreply.github.com>
2021-04-14 21:42:53 +0200

    Setup react app
    Signed-off-by: pieterh <5267445+pieterh@users.noreply.github.com>

---
+++ backend/EMS.WebHost/Controllers/UsersController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using EMS.WebHost.Helpers;
+
+namespace EMS.WebHost.Controllers
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+    }
+
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private ILogger Logger => _logger;
+        private readonly JwtTokens _jwtCreator;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+            var settings = new JwtSettings() {
+                Key = "72cc7881-297d-4670-8d95-54a00692f1ab",
+                Issuer = "http://petteflet.org",
+                Audience = "Test",
+                MinutesToExpiration = 5
+            };
+
+
+            _jwtCreator = new JwtTokens(settings);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] LoginModel model)
+        {
+            var user = PerformAuth(model);
+            if (user != null)
+            {
+                var token = _jwtCreator.Generate(user.Id, user.Username, user.Name);
+                Response.Cookies.Append("X-Access-Token", token, new CookieOptions() { Path = "/api", HttpOnly = true, SameSite = SameSiteMode.Strict });
+                return Ok(user);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+        
+        [HttpGet("ping")]
+        public string Ping() {
+            var id = User.Claims.FirstOrDefault<Claim>((x) => x.Type == JwtRegisteredClaimNames.Sub)?.Value;
+            var name = User.Claims.FirstOrDefault<Claim>((x) => x.Type == "name")?.Value;
+            var s = new StringBuilder();
+            foreach(var c in User.Claims)
+            {
+                _logger.LogDebug($"{c.Type} - {c.Value}");
+            }
+            return $"pong{Environment.NewLine}Hello {name}";
+        }
+
+        private UserModel PerformAuth(LoginModel model)
+        {
+            var user = (model.Username.Equals("admin") &&
+                    model.Password.Equals("admin")) ? new UserModel
+                    {
+                        Id = Guid.NewGuid(),
+                        Username = "admin",
+                        Name = "Pieter Hilkemeijer"
+                    } : null;
+            return user;
+        }
+    }
+}
---
+++ backend/EMS/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using CommandLine;
+using NLog.Extensions.Logging;
+using EMS.Library.Configuration;
+using EMS.WebHost;
+
+namespace EMS
+{
+    static class Program
+    {
+        public class Options
+        {
+            [Option('c', "config", Required = true, HelpText = "filename of config")]
+            public string ConfigFile { get; set; }
+            [Option('l', "nlogcfg", Required = false, HelpText = "filename of the nlog file")]
+            public string NLogConfig { get; set; }
+        }
+
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static async Task Main(string[] args)
+        {
+            Options options = new();
+
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
+            {
+                options = o;
+            });
+
+            try
+            {
+                await CreateHost(options).RunAsync();
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+        }
+
+        static IHost CreateHost(Options options)
+        {
+            
+            var t = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((builderContext, configuration) =>
+                {
+                    IHostEnvironment env = builderContext.HostingEnvironment;
+                    Logger.Info($"Hosting environment: {env.EnvironmentName}");
+
+                    if (ConfigurationManager.ValidateConfig(options.ConfigFile))
+                    {
+                        configuration.Sources.Clear();
+                        
+                        configuration
+                           .AddJsonFile(options.ConfigFile, optional: false, reloadOnChange: false)
+                           .AddJsonFile($"config.{env.EnvironmentName}.json", optional: true, reloadOnChange: false);
+                    }
+                    else
+                        Logger.Error("There was an error with the configuration file");
+
+                })
+                .ConfigureLogging((ILoggingBuilder logBuilder) =>
+                {
+                    logBuilder.ClearProviders();
+                    logBuilder.SetMinimumLevel(LogLevel.Trace);
+                    logBuilder.AddNLog(options.NLogConfig);
+                })
+                .ConfigureServices((builderContext, services) =>
+                {
+
+                    //services.Configure<List<Adapter>>(hostingContext.Configuration.GetSection("adapters"));
+                    //services.Configure<List<Instance>>(hostingContext.Configuration.GetSection("instances"));
+                    //services.Configure<WebConfig>(hostingContext.Configuration.GetSection("web"));
+
+
+                    ConfigureInstances(builderContext, services);
+
+                    services.AddSingleton<IHostedService>(x => ActivatorUtilities.CreateInstance<HEMSCore>(x));
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel((builderContext, kestrelOptions) =>
+                    {
+                        //kestrelOptions.Configure(
+                        // builderContext.Configuration.GetSection("Kestrel"), reloadOnChange: false);
+
+                        WebConfig wc = new();
+                        builderContext.Configuration.GetSection("web").Bind(wc);
+
+                        kestrelOptions.ListenLocalhost(wc.Port, builder =>
+                        {
+                            if (!builderContext.HostingEnvironment.IsDevelopment())
+                                builder.UseHttps();
+                        });
+                    });
+
+                    webBuilder.UseStartup<Startup>();
+                }).Build();
+
+            return t;
+        }
+
+        private static void ConfigureInstances(HostBuilderContext hostingContext, IServiceCollection services)
+        {
+            var adapters = new List<Adapter>();
+            hostingContext.Configuration.GetSection("adapters").Bind(adapters);
+
+            var instances = new List<Instance>();
+            hostingContext.Configuration.GetSection("instances").Bind(instances);
+
+            foreach (var instance in instances)
+            {
+                Logger.Debug($"Instance [{instance.Name}]");
+                var adapter = GetAdapter(adapters, instance.AdapterId);
+                Logger.Debug($"Instance [{instance.Name}], loading assembly {adapter.Driver.Assembly}");
+
+                var adapterAssembly = Assembly.Load(adapter.Driver.Assembly);
+                Logger.Debug($"Instance [{instance.Name}], loaded assembly from location {adapterAssembly.Location}");
+
+                var adapterType = adapterAssembly.GetType(adapter.Driver.Type);
+                Logger.Debug($"Instance [{instance.Name}], type {adapterType.FullName} loaded");
+
+                Logger.Debug($"Instance [{instance.Name}], configuring services");
+                adapterType.GetMethod("ConfigureServices", BindingFlags.Static | BindingFlags.Public)
+                                .Invoke(null, new object[] { hostingContext, services, instance });
+                Logger.Debug($"Instance [{instance.Name}], configuring services done");
+            }
+        }
+
+        public static Adapter GetAdapter(List<Adapter> adapters, Guid adapterid)
+        {
+            foreach (var adapter in adapters)
+            {
+                if (adapter.Id == adapterid)
+                    return adapter;
+            }
+            return null;
+        }
+    }
+
+}
+
+
