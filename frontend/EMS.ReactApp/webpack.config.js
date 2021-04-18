6b42d59e08f578d5434fc2121862ce6daf82badc
pieterh <5267445+pieterh@users.noreply.github.com>
2021-04-18 23:26:37 +0200

    react with typescript

--- frontend/EMS.ReactApp/webpack.config.js
+++ frontend/EMS.ReactApp/webpack.config.js
@@ -1,20 +1,36 @@
 ﻿path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InterpolateHtmlPlugin = require('interpolate-html-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const publicUrl = '';
 
 module.exports = {
     entry: {
-        index: './src/index.js',
+        index: './src/index.tsx',
     },
-    plugins: [
+    plugins: [        
+        new InterpolateHtmlPlugin({
+            PUBLIC_URL: publicUrl,
+        }),
         new HtmlWebpackPlugin({
             title: 'ReactApp',
             favicon: 'public/favicon.ico',
             template: 'public/index.html',
         }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public/logo192.png', to: 'dist' },
+            ]
+        })
     ],
     output: {
-        filename: '[name].bundle.[hash].js',
+        filename: '[name].bundle.[chunkhash].js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: "/",
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
     },
     module: {
         rules: [
@@ -23,6 +39,11 @@ module.exports = {
                 exclude: /node_modules/,
                 use: ['babel-loader']
             },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },            
             {
                 test: /\.css$/i,
                 use: [
@@ -38,6 +59,21 @@ module.exports = {
                     }
                 ],
             },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                      loader: 'svg-url-loader',
+                      options: {
+                        limit: 10000,
+                      },
+                    },
+                  ],
+            },
+            {
+                test   : /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader : 'file-loader'
+            }
         ],
     },
     mode: 'development',
