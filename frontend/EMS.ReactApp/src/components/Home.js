18aa5ca4d785265686691ef65ce241be679ef50b
pieterh <5267445+pieterh@users.noreply.github.com>
2021-04-18 10:09:19 +0200

    refactor directory structure

---
+++ frontend/EMS.ReactApp/src/components/Home.js
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Layout from './Layout';
+
+const Home = () => {
+    return (
+        <Layout>
+            <p>Hello World "React and Webpack!"</p>
+            <p>
+                <Link to="/dynamic">Navigate to Dynamic Page</Link>
+            </p>
+        </Layout>
+    );
+};
+
+export default Home;
---
+++ frontend/EMS.ReactApp/webpack.config.js
@@ -0,0 +1,50 @@
+﻿path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'ReactApp',
+            favicon: 'public/favicon.ico',
+            template: 'public/index.html',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: true
+                        }
+                    }
+                ],
+            },
+        ],
+    },
+    mode: 'development',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 5010,
+        https: false,
+    },
+};
