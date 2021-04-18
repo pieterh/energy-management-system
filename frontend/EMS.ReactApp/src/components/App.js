57c010012e126e2b1c2d4fcd2d2fb252235e5c5f
pieterh <5267445+pieterh@users.noreply.github.com>
2021-04-18 10:09:19 +0200

    refactor directory structure

---
+++ frontend/EMS.ReactApp/src/components/App.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './Home';
+import DynamicPage from './DynamicPage';
+import NoMatch from './NoMatch';
+
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/dynamic" component={DynamicPage} />
+                    <Route component={NoMatch} />
+                </Switch>
+            </div>
+        </Router>
+    );
+};
+
+export default App;
---
+++ frontend/EMS.ReactApp/src/index.js
@@ -0,0 +1,5 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
