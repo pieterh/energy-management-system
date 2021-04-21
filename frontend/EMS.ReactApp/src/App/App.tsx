7436829019c85ce1966f340b51f6bdc97676d6b5
pieterh <5267445+pieterh@users.noreply.github.com>
2021-04-22 00:35:07 +0200

    adding dummy login component

--- frontend/EMS.ReactApp/src/App/App.tsx
+++ frontend/EMS.ReactApp/src/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Main from '../components/Main/Main';
@@ -8,12 +8,12 @@ import Login from '../components/Login/Login';
 function App() {
   return (
     <div className="App">
-      <Router>
-          <Link to={'/app/dashboard'}>dashbaord</Link>
+      <BrowserRouter basename="/app">
+          <Link to={'/dashboard'}>dashboard</Link>
           <Route path='/' render={() => (<h1>Welcome ;-)</h1>)}></Route>
-          <Route path='/app/dashboard' render={() => (<h1>Welcome ...</h1>)}></Route>
-          <Route path='/app/login' component='Login'/>        
-      </Router>
+          <Route path='/dashboard' render={() => (<h1>Welcome ***</h1>)}></Route>
+          <Route path='/login'> <Login/> </Route>      
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/EMS.ReactApp/src/components/Login/Login.tsx
+++ frontend/EMS.ReactApp/src/components/Login/Login.tsx
@@ -1,23 +1,136 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { createStyles, withStyles, Theme } from '@material-ui/core/styles';
+
+import Container from '@material-ui/core/Container';
 import './Login.css';
 
-export default function Login() {
-  return(
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form>
-        <label>
-          <p>Username</p>
-          <input type="text" />
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  )
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
+
+const styles = ({ palette, spacing }: Theme) => createStyles({
+  paper: {
+    marginTop: spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: spacing(1),
+    backgroundColor: palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: spacing(1),
+  },
+  submit: {
+    margin: spacing(3, 0, 2),
+  },
+});
+
+interface ILoginProps {
+  classes: any
+}
+
+interface ILoginState {
+}
+
+class Login extends React.Component<ILoginProps, ILoginState> {
+  constructor(props: ILoginProps) {
+      super(props);
+
+      this.state = {
+          materialUIclasses: {}
+      }
+  }
+
+  render() {
+    return(
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={this.props.classes.paper}>
+          <Avatar className={this.props.classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={this.props.classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={this.props.classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    )
+  }
+}
+
+export default withStyles(styles)(Login)
