c6ec5850243dff4d86f772f01ea918fee230eb83
pieterh <5267445+pieterh@users.noreply.github.com>
2021-05-21 15:04:22 +0200

    start using the connected react router
    Signed-off-by: pieterh <5267445+pieterh@users.noreply.github.com>

--- frontend/EMS.ReactApp/src/app/store.ts
+++ frontend/EMS.ReactApp/src/app/store.ts
@@ -1,20 +1,26 @@
 import { combineReducers } from 'redux';
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { createBrowserHistory, History } from 'history'
 
 import drawerReducer from '../features/appdrawer/drawerSlice';
 import authenticationReducer from '../features/authentication/authenticationSlice';
 import evseReducer from '../features/chargepoint/EVSESlice';
 import customThemeProviderReducer from '../features/themeprovider/CustomThemeProviderSlice';
 
-const rootReducer = combineReducers({
-    drawer: drawerReducer,
-    authentication: authenticationReducer,
-    evse: evseReducer,
-    customTheme: customThemeProviderReducer
-  });
+export const history = createBrowserHistory();
+
+const createRootReducer = (history: History) => combineReducers({
+  router: connectRouter(history),
+  drawer: drawerReducer,
+  authentication: authenticationReducer,
+  evse: evseReducer,
+  customTheme: customThemeProviderReducer
+});
 
 export const store = configureStore({
-  reducer: rootReducer,    
+  reducer: createRootReducer(history),        
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(routerMiddleware(history)),
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
