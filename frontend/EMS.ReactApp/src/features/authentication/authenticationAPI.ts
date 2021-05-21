4c153cf880171c85aa73771de881ab2bc20c3cda
pieterh <5267445+pieterh@users.noreply.github.com>
2021-05-21 19:55:53 +0200

    clear redux state after logout or authentication error
    Signed-off-by: pieterh <5267445+pieterh@users.noreply.github.com>

--- frontend/EMS.ReactApp/src/features/authentication/authenticationAPI.ts
+++ frontend/EMS.ReactApp/src/features/authentication/authenticationAPI.ts
@@ -1,28 +1,10 @@
-// export interface GeneralResponse {
-//      ok: boolean,
-//      message: string
-// }
-
-// export class LoginResponse implements GeneralResponse {
-//     public constructor(
-//         public ok: boolean,
-//         public message: string
-//     ){
-
-//     }
-// }
-
-// export class LoginResponse {
-//      ok: boolean;
-//       message: string;
-//  }
 
 // A mock function to mimic making an async request for data
 export function login(username :string, secret: string) {
     return new Promise<{ data: any }>((resolve) => {
         console.info("login()");     
         var resp = { ok: true }
-        setTimeout(() => resolve({ data: resp }), 2500)
+        setTimeout(() => resolve({ data: resp }), 500)
     });
   }
 
@@ -31,7 +13,7 @@ export function logout() {
     return new Promise<{ data: any }>((resolve) => {
         console.info("logout()");   
         var resp = { ok: true }
-        setTimeout(() => resolve({ data: true }), 2500)
+        setTimeout(() => resolve({ data: true }), 500)
     });
   }
 
