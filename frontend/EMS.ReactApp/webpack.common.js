79f91ea877c089a6a5c277f1dfc8dc2461835273
pieterh <5267445+pieterh@users.noreply.github.com>
2021-06-02 15:05:52 +0200

    upgrade packages and browserslist
    Signed-off-by: pieterh <5267445+pieterh@users.noreply.github.com>

--- frontend/EMS.ReactApp/webpack.common.js
+++ frontend/EMS.ReactApp/webpack.common.js
@@ -1,55 +1,69 @@
-path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const InterpolateHtmlPlugin = require('interpolate-html-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import InterpolateHtmlPlugin from 'interpolate-html-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const publicUrl = '/app';
+export const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-let plugins = [
-    new CopyWebpackPlugin({
-        patterns: [
-            { from: 'public/logo192.png', to: '' },
-            { from: 'public/logo512.png', to: '' },
-            { from: 'public/app.webmanifest', to: '' },
-        ]
-    }),
-    new InterpolateHtmlPlugin({
-        PUBLIC_URL: publicUrl,
-    }),        
-    new HtmlWebpackPlugin({
-        title: 'ReactApp',
-        favicon: 'public/favicon.ico',
-        template: 'public/index.html',
-    }),
-];
-
-module.exports = {
-    entry: [
+export const config = {
+    plugins : [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public/logo192.png', to: '' },
+                { from: 'public/logo512.png', to: '' },
+                { from: 'public/app.webmanifest', to: '' },
+            ]
+        }),
+        new InterpolateHtmlPlugin({
+            PUBLIC_URL: publicUrl,
+        }),        
+        new HtmlWebpackPlugin({
+            title: 'ReactApp',
+            favicon: 'public/favicon.ico',
+            template: 'public/index.html',
+        }),
+    ],
+    entry : [
         './src/index.tsx',
     ],
-    plugins: plugins,
-    target: 'web',
-    output: {
+    target : 'browserslist',
+    output : {
         filename: '[name].bundle.[chunkhash].js',
         path: path.resolve(__dirname, 'dist/app'),
         publicPath: "/app/",
         clean: true
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+    resolve : {
+        extensions: ['.tsx', '.ts', '.js', '.mjs'],
     },
-    module: {
+    module : {
         rules: [
             {
                 test: /\.(js)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                resolve: {
+                    fullySpecified: false
+                }
             },
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: {
+                    loader: 'ts-loader',
+                },
                 exclude: /node_modules/,
-            },            
+            },
             {
                 test: /\.css$/i,
                 use: [
@@ -69,16 +83,16 @@ module.exports = {
                 test: /\.svg$/,
                 use: [
                     {
-                      loader: 'svg-url-loader',
-                      options: {
-                        limit: 10000,
-                      },
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
                     },
-                  ],
+                ],
             },
             {
-                test   : /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-                loader : 'file-loader'
+                test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader: 'file-loader'
             }
         ],
     }
