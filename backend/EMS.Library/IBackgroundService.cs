94d58c6c27e8b5c9f4b7602912c75cc10663f4c1
pieterh <5267445+pieterh@users.noreply.github.com>
2021-06-14 21:45:29 +0200

    some fixes and start refactor
    Signed-off-by: pieterh <5267445+pieterh@users.noreply.github.com>

---
+++ backend/EMS.Library/IBackgroundService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace EMS.Library
+{
+    public interface IBackgroundService: IHostedService, IDisposable
+    {
+    }
+}
--- backend/EMS.Tests/EngineStateMachine.Tests.cs
+++ backend/EMS.Tests/EngineStateMachine.Tests.cs
@@ -2,6 +2,7 @@
 using EMS.Engine;
 using EMS.Library;
 using EMS.Library.Core;
+using EMS.Library.TestableDateTime;
 using FluentAssertions;
 using Xunit;
 using static EMS.Engine.ChargingStateMachine;
@@ -10,68 +11,66 @@ namespace EMS.Tests
 {
     public class EngineStateMachine
     {
-        const int MIN_DELAY = ChargingStateMachine.MINIMUM_TIME_SECS;
-        const int DELAY = MIN_DELAY + 15;
-        const int DELAY_SMALL = DELAY / 2;
+        private int MIN_DELAY;
+        private int DELAY;
+        private int DELAY_SMALL;
+        private ChargingStateMachine _stateMachine;
+
+        public EngineStateMachine()
+        {
+            _stateMachine = new ChargingStateMachine
+            {
+                LastStateChange = DateTimeProvider.Now
+            };
+
+            MIN_DELAY = _stateMachine.MinimumTransitionTime;
+            DELAY = MIN_DELAY + 15;
+            DELAY_SMALL = DELAY / 2;
+        }
 
         [Fact]
         public void Test1()
         {
-            var stateMachine = new ChargingStateMachine
-            {
-                LastStateChange = DateTime.Now
-            };
-
-            stateMachine.Start();
-            stateMachine.Current.Should().Be(ChargingState.Charging, "we can start immediatly after creation");
-            stateMachine.Stop();
-            stateMachine.Current.Should().Be(ChargingState.NotCharging, "we can stop immediatly after starting");
-            stateMachine.Start();
-            stateMachine.Current.Should().Be(ChargingState.Charging, "we can start immediatly after stopping");
+            _stateMachine.Start();
+            _stateMachine.Current.Should().Be(ChargingState.Charging, "we can start immediatly after creation");
+            _stateMachine.Stop();
+            _stateMachine.Current.Should().Be(ChargingState.NotCharging, "we can stop immediatly after starting");
+            _stateMachine.Start();
+            _stateMachine.Current.Should().Be(ChargingState.Charging, "we can start immediatly after stopping");
         }
 
         [Fact]
         public void Test2()
         {
-            var stateMachine = new ChargingStateMachine
-            {
-                LastStateChange = DateTime.Now
-            };
-
-            stateMachine.Start();
-            stateMachine.Current.Should().Be(ChargingState.Charging, "we can start immediatly after creation");
-            stateMachine.LastStateChange = DateTime.Now.AddSeconds(-DELAY);
-            stateMachine.Pause();
-            stateMachine.Current.Should().Be(ChargingState.ChargingPaused, "after some time we should be able to pause");
-            stateMachine.Stop();
-            stateMachine.Current.Should().Be(ChargingState.NotCharging, "we can stop immediatly even when paused");            
+            _stateMachine.Start();
+            _stateMachine.Current.Should().Be(ChargingState.Charging, "we can start immediatly after creation");
+            _stateMachine.LastStateChange = DateTime.Now.AddSeconds(-DELAY);
+            _stateMachine.Pause();
+            _stateMachine.Current.Should().Be(ChargingState.ChargingPaused, "after some time we should be able to pause");
+            _stateMachine.Stop();
+            _stateMachine.Current.Should().Be(ChargingState.NotCharging, "we can stop immediatly even when paused");            
         }
 
         [Fact]
         public void Test3()
         {
-            var stateMachine = new ChargingStateMachine
-            {
-                LastStateChange = DateTime.Now
-            };
+            _stateMachine.Start();
+            _stateMachine.Current.Should().Be(ChargingState.Charging, "we can start immediatly after creation");
+            _stateMachine.Pause();
+            _stateMachine.Current.Should().Be(ChargingState.Charging, "we can't pause immediatly after starting");
+            _stateMachine.LastStateChange = DateTime.Now.AddSeconds(-DELAY_SMALL);
+            _stateMachine.Pause();
+            _stateMachine.Current.Should().Be(ChargingState.Charging, "we can't pause immediatly after starting");
 
-            stateMachine.Start();
-            stateMachine.Current.Should().Be(ChargingState.Charging, "we can start immediatly after creation");
-            stateMachine.Pause();
-            stateMachine.Current.Should().Be(ChargingState.Charging, "we can't pause immediatly after starting");
-            stateMachine.LastStateChange = DateTime.Now.AddSeconds(-DELAY_SMALL);
-            stateMachine.Pause();
-            stateMachine.Current.Should().Be(ChargingState.Charging, "we can't pause immediatly after starting");
-
-            stateMachine.LastStateChange = DateTime.Now.AddSeconds(-DELAY);
-            stateMachine.Pause();
-            stateMachine.Current.Should().Be(ChargingState.ChargingPaused, "after some time we should be able to");
-            stateMachine.LastStateChange = DateTime.Now.AddSeconds(-DELAY_SMALL);
-            stateMachine.Unpause();
-            stateMachine.Current.Should().Be(ChargingState.ChargingPaused, "we can't unpause immediatly");
-            stateMachine.LastStateChange = DateTime.Now.AddSeconds(-DELAY);
-            stateMachine.Unpause();
-            stateMachine.Current.Should().Be(ChargingState.Charging, "after some time we should be able to");
+            _stateMachine.LastStateChange = DateTime.Now.AddSeconds(-DELAY);
+            _stateMachine.Pause();
+            _stateMachine.Current.Should().Be(ChargingState.ChargingPaused, "after some time we should be able to");
+            _stateMachine.LastStateChange = DateTime.Now.AddSeconds(-DELAY_SMALL);
+            _stateMachine.Unpause();
+            _stateMachine.Current.Should().Be(ChargingState.ChargingPaused, "we can't unpause immediatly");
+            _stateMachine.LastStateChange = DateTime.Now.AddSeconds(-DELAY);
+            _stateMachine.Unpause();
+            _stateMachine.Current.Should().Be(ChargingState.Charging, "after some time we should be able to");
         }
     }
 }
