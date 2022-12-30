adc8cbefed2f80d01c41167aa70ba654e821c7cd
pieterh <5267445+pieterh@users.noreply.github.com>
2022-12-30 12:48:13 +0100

    Upgrading some packages

--- backend/EMS.WebHost/Controllers/DemoController.cs
+++ backend/EMS.WebHost/Controllers/DemoController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.Docs.Samples;
 using Microsoft.Extensions.Logging;
 
 namespace EMS.WebHosts
