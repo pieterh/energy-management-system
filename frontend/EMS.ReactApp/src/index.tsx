03795ec193cb87134946da506dd0cd5f1c49242c
pieterh <5267445+pieterh@users.noreply.github.com>
2022-12-30 21:39:50 +0100

    Develop (#652)
    * Upgrade from .NET 6 to .NET 7
    
    * Upgraded some packages
    
    * Update client rendering api due to react 18 upgrade
    
    * Force react strictmode again and fix app header (menu)

--- frontend/EMS.ReactApp/src/index.tsx
+++ frontend/EMS.ReactApp/src/index.tsx
@@ -1,4 +1,5 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ConnectedRouter } from 'connected-react-router'
@@ -8,19 +9,19 @@ import App from './app/App';
 
 import { AddInterceptors } from './features/authentication/AxiosInterceptors';
 
+const container = document.getElementById('root')
+const root = createRoot(container!); 
+
 // make sure to add the interceptors _after_ creating the store and _before_ rendering anything.
 // This will make sure that all http calls can be intercepted while initially drawing the components.
 AddInterceptors();
 
-ReactDOM.render(
-  // Strict mode is not working properly with material-ui (in development mode)
-  // ie. toggle theme creates each time a set of styles in the header
-  // <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-     </BrowserRouter>
-   </Provider>,
-  // </React.StrictMode>,
-  document.getElementById('root')
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
