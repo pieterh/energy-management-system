9d753ca55542b228819333a1c4b8e38e46d08fd6
pieterh <5267445+pieterh@users.noreply.github.com>
2022-12-30 21:20:16 +0100

    Update client rendering api due to react 18 upgrade

--- frontend/EMS.ReactApp/src/index.tsx
+++ frontend/EMS.ReactApp/src/index.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ConnectedRouter } from 'connected-react-router'
@@ -8,11 +8,14 @@ import App from './app/App';
 
 import { AddInterceptors } from './features/authentication/AxiosInterceptors';
 
+const container = document.getElementById('root')
+const root = createRoot(container!); 
+
 // make sure to add the interceptors _after_ creating the store and _before_ rendering anything.
 // This will make sure that all http calls can be intercepted while initially drawing the components.
 AddInterceptors();
 
-ReactDOM.render(
+root.render(
   // Strict mode is not working properly with material-ui (in development mode)
   // ie. toggle theme creates each time a set of styles in the header
   // <React.StrictMode>
@@ -21,6 +24,5 @@ ReactDOM.render(
       <App />
      </BrowserRouter>
    </Provider>,
-  // </React.StrictMode>,
-  document.getElementById('root')
+  // </React.StrictMode>,  
 );
