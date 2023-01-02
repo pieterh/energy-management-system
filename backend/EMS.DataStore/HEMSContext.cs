7298f8a21c73d4291bb1bec8c73315de09bf271d
pieterh <5267445+pieterh@users.noreply.github.com>
2023-01-02 17:36:21 +0100

    Some cleanup

--- backend/EMS.DataStore/HEMSContext.cs
+++ backend/EMS.DataStore/HEMSContext.cs
@@ -7,10 +7,13 @@ namespace EMS.DataStore
 	public class HEMSContext : DbContext
 	{
         public string DbPath { get; }
+// Disable nullable warning, since the entity framework will set the property ChargingTransactions
+#pragma warning disable CS8618
         public DbSet<ChargingTransaction> ChargingTransactions { get; set; }
 
         public HEMSContext()
         {
+#pragma warning restore CS8618
             var folder = AppDomain.CurrentDomain.BaseDirectory;
             DbPath = System.IO.Path.Join(folder, "hems.db");
         }
