acf2d3ee474167ce0fa745a36d5ddf320a399257
Pieter Hilkemeijer <5267445+pieterh@users.noreply.github.com>
2023-04-24 14:50:10 +0200

    FileTools unit tests added

---
+++ backend/EMS.Library.Unit.Tests/FileTools.Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentAssertions;
+
+using EMS.Library.Files;
+
+namespace FileToolsUnitTest
+{
+    public class FileToolsTests
+    {
+        [Fact]
+        public void FileExistsAndReadableNullAndEmptyAreFalse()
+        {
+            FileTools.FileExistsAndReadable(null).Should().BeFalse();
+            FileTools.FileExistsAndReadable(string.Empty).Should().BeFalse();
+        }
+
+        [Fact]
+        public void FileExistsAndReadableNotExistingFileIsFalse()
+        {
+            FileTools.FileExistsAndReadable("doesnt exist").Should().BeFalse();
+        }
+
+        [Fact]
+        public void FileExistsAndReadableExistingFileIsTrue()
+        {
+            var f = typeof(FileToolsTests).Assembly.Location;
+            FileTools.FileExistsAndReadable(f).Should().BeTrue();
+        }
+
+    }
+}
+
