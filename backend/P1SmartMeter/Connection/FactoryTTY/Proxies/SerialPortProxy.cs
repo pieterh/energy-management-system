92235ec0e3a705947fff31693d7f7f8b0f0eef5c
Pieter Hilkemeijer <5267445+pieterh@users.noreply.github.com>
2023-04-26 23:25:05 +0200

    P1 Connection refactor and implement more unit tests for it

---
+++ backend/P1SmartMeter/Connection/FactoryTTY/Proxies/SerialPortProxy.cs
@@ -0,0 +1,12 @@
+﻿using System.IO.Ports;
+using P1SmartMeter.Connection.Proxies;
+
+namespace P1SmartMeter.Connection.FactoryTTY.Proxies
+{
+    internal class SerialPortProxy : SerialPort, ISerialPort
+    {
+        public SerialPortProxy(string portName) : base(portName)
+        {
+        }
+    }
+}
--- backend/P1SmartMeter/Connection/P1ReaderTTY.cs
+++ backend/P1SmartMeter/Connection/P1ReaderTTY.cs
@@ -1,7 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.IO.Ports;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO.Ports;
+using P1SmartMeter.Connection.Factories;
+using P1SmartMeter.Connection.Proxies;
 
 namespace P1SmartMeter.Connection
 {
@@ -11,18 +10,21 @@ namespace P1SmartMeter.Connection
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
 
         private readonly string _usbPort;
-        private SerialPort? _serialPort;
+        private readonly ISerialPortFactory _serialPortFactory;
 
-        public P1ReaderTTY(string deviceName)
+        private ISerialPort? _serialPort;
+
+        public P1ReaderTTY(string deviceName, ISerialPortFactory? serialPortFactory = null)
         {
             Logger.Trace($"Availble ports ->");
 
-            SerialPort.GetPortNames().ToList().ForEach(x =>
+            _usbPort = deviceName;
+            _serialPortFactory = serialPortFactory ?? new SerialPortFactory();
+
+            _serialPortFactory.GetPortNames().ToList().ForEach(x =>
             {
                 Logger.Trace($"Port '{x}'");
             });
-
-            _usbPort = deviceName;
         }
 
         protected override void Dispose(bool disposing)
@@ -45,15 +47,14 @@ namespace P1SmartMeter.Connection
         {
             Logger.Info($"BackgroundTask start");
 
-            _serialPort = new SerialPort(_usbPort)
-            {
-                BaudRate = 115200,
-                Parity = Parity.None,
-                StopBits = StopBits.One,
-                DataBits = 8,
-                ReadTimeout = 30000,
-                ReadBufferSize = 4096
-            };
+            _serialPort = _serialPortFactory.CreateSerialPort(_usbPort);
+            _serialPort.BaudRate = 115200;
+            _serialPort.Parity = Parity.None;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.DataBits = 8;
+            _serialPort.ReadTimeout = 30000;
+            _serialPort.ReadBufferSize = 409;
+
             _serialPort.DataReceived += Str_DataReceived;
             _serialPort.Open();
 
@@ -69,7 +70,7 @@ namespace P1SmartMeter.Connection
         private void Str_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             Logger.Info($"DataReceived!");
-            SerialPort sp = (SerialPort)sender;
+            ISerialPort sp = (ISerialPort)sender;
             string indata = sp.ReadExisting();
             Logger.Info($"BackgroundTask read {indata.Length} characters...");
 
