2345053f5b6f918eede793baf8a1e2146b32a989
Pieter Hilkemeijer <5267445+pieterh@users.noreply.github.com>
2023-05-11 06:47:49 +0200

    Frontend warning fix

--- frontend/EMS.BlazorWasm/Services/Auth/IUserService.cs
+++ frontend/EMS.BlazorWasm/Services/Auth/IUserService.cs
@@ -7,8 +7,8 @@ namespace EMS.BlazorWasm.Client.Services.Auth
 {
     public interface IUserService
     {
-        Task<Response?> LoginAsync(LoginModel model);
-        Task<Response?> LoginAsync(LoginModel model, CancellationToken cancellationToken);
+        Task<Response> LoginAsync(LoginModel model);
+        Task<Response> LoginAsync(LoginModel model, CancellationToken cancellationToken);
         Task LogoutAsync();
         Task<SetPasswordResponse> SetPasswordAsync(SetPasswordModel model, CancellationToken cancellationToken);
         Task<PingResponse> Ping();
